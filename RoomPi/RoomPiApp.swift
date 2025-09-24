98777c199f1e381c2495c1ade8f09a2c491cb299
michalx106 <michalx106@gmail.com>
2025-09-24 20:29:05 +0200

    Initial Commit

---
+++ RoomPi/RoomPiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomPiApp.swift
+//  RoomPi
+//
+//  Created by Michał Grzesiewicz on 24/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct RoomPiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
